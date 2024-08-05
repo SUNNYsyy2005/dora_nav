9dfddcdf3c25eca12b5a1553b0eb66238d4985f6
starlitxiling <1754165401@qq.com>
2024-08-05 09:21:27 +0800

    fix dora error

--- build/lidar/build/CMakeFiles/lslidar_cx_input.dir/DependInfo.cmake
+++ build/lidar/build/CMakeFiles/lslidar_cx_input.dir/DependInfo.cmake
@@ -8,11 +8,15 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/sunny/dora_nav/build/lidar/src/input.cc" "CMakeFiles/lslidar_cx_input.dir/src/input.cc.o" "gcc" "CMakeFiles/lslidar_cx_input.dir/src/input.cc.o.d"
+  "/home/xiling/Desktop/1/dora_nav/build/lidar/src/input.cc" "CMakeFiles/lslidar_cx_input.dir/src/input.cc.o" "gcc" "CMakeFiles/lslidar_cx_input.dir/src/input.cc.o.d"
   )
 
-# Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
