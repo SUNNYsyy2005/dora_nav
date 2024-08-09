a9b144981b78039df8f64877f319b6045bf98da3
SUNNYsyy2005 <s70735526@163.com>
2024-08-09 17:25:33 +0800

    finished imu node

--- build/imu/main.cc
+++ build/imu/main.cc
@@ -2,132 +2,138 @@ extern "C"
 {
 #include "node_api.h"
 }
- 
+
 #include <iostream>
-#include <vector>
+#include <string>
 #include <cmath>
-#include <serial/serial.h>
+#include <vector>
+#include <boost/asio.hpp>
 #include <nlohmann/json.hpp>
 #include "../../include/ros.h"
- 
-const std::string serial_port = "/dev/ttyUSB1";
-const unsigned long serial_baud = 115200;
- 
-int H_(unsigned char data) {
-    int re = data * 256;
+
+using namespace std;
+using namespace boost::asio;
+using json = nlohmann::json;
+
+io_service io;
+serial_port imu(io, "/dev/ttyUSB0");
+
+// 全局变量
+double imu_yaw_calibration = 0.0;
+double roll = 0;
+double pitch = 0;
+double yaw = 0;
+int seq = 0;
+vector<double> euler(3, 0);
+double accel_factor = 9.806 / 256.0;
+double degrees2rad = M_PI / 180.0;
+
+sensor_msgs::Imu imu_msg;
+
+
+// 辅助函数
+int H_(char data) {
+    int re = static_cast<unsigned char>(data) * 256;
     return re > 32767 ? re - 65536 : re;
 }
- 
-int L_(unsigned char data) {
-    return data;
-}
- 
-bool to_exit_process;
-int message_count = 0; // 消息计数器
-const int max_messages = 10000; // 最大消息数
- 
-int run(void *dora_context)
-{
-    to_exit_process = false;
-    serial::Serial IMU;
- 
-    // Initialize serial port
-    try {
-        IMU.setPort(serial_port);
-        IMU.setBaudrate(serial_baud);
-        serial::Timeout timeout = serial::Timeout::simpleTimeout(1000);
-        IMU.setTimeout(timeout);
-        IMU.open();
-        if (IMU.isOpen()) {
-            IMU.write("\xA5\x5A\x04\x01\x05\xAA\n");
-        } else {
-            std::cerr << "Failed to open serial port" << std::endl;
-            return -1;
-        }
-    } catch (serial::IOException& e) {
-        std::cerr << "Serial Exception: " << e.what() << std::endl;
-        return -1;
-    }
- 
-    while (!to_exit_process && message_count < max_messages)
-    {
-        void *event = dora_next_event(dora_context);
-        if (event == NULL)
-        {
-            printf("[c node] ERROR: unexpected end of event\n");
-            return -1;
-        }
- 
-        enum DoraEventType ty = read_dora_event_type(event);
- 
-        if (ty == DoraEventType_Input)
-        {
-            std::vector<unsigned char> buffer(256);
-            size_t len = IMU.read(buffer, buffer.size());
- 
-            if (len > 0 && buffer[0] == 0xA5 && buffer[1] == 0x5A && buffer[len - 1] == 0xAA) {
-                sensor_msgs::Imu imu;
- 
-                double yaw_deg = (H_(buffer[3]) + L_(buffer[4])) / 10.0;
-                if (yaw_deg > 180.0) yaw_deg -= 360.0;
-                if (yaw_deg < -180.0) yaw_deg += 360.0;
- 
-                imu.orientation.x = yaw_deg * M_PI / 180.0;
-                imu.orientation.y = (H_(buffer[7]) + L_(buffer[8])) / 10.0 * M_PI / 180.0;
-                imu.orientation.z = (H_(buffer[5]) + L_(buffer[6])) / 10.0 * M_PI / 180.0;
-                imu.orientation.w = 1.0; // assuming w as 1.0 for this example
- 
-                imu.linear_acceleration.x = 9.806 * (H_(buffer[9]) + L_(buffer[10])) / 16384.0;
-                imu.linear_acceleration.y = 9.806 * (H_(buffer[11]) + L_(buffer[12])) / 16384.0;
-                imu.linear_acceleration.z = 9.806 * (H_(buffer[13]) + L_(buffer[14])) / 16384.0;
-                imu.angular_velocity.x = (H_(buffer[15]) + L_(buffer[16])) / 32.8 * M_PI / 180.0;
-                imu.angular_velocity.y = (H_(buffer[17]) + L_(buffer[18])) / 32.8 * M_PI / 180.0;
-                imu.angular_velocity.z = (H_(buffer[19]) + L_(buffer[20])) / 32.8 * M_PI / 180.0;
- 
-                nlohmann::json json_obj = imu.to_json();
-                std::string json_str = json_obj.dump();
-                //printf("%s\n", json_str.c_str());
-                const char* char_ptr = json_str.c_str();
-                char* non_const_char_ptr = new char[json_str.size() + 1];
-                std::memcpy(non_const_char_ptr, char_ptr, json_str.size() + 1);
 
-                std::string out_id = "data";
-                int result = dora_send_output(dora_context, &out_id[0], out_id.length(), reinterpret_cast<char*>(non_const_char_ptr), json_str.size());
-                if (result != 0)
-                {
-                    std::cerr << "failed to send output" << std::endl;
-                    return 1;
-                }
-                message_count++; // 递增消息计数器
-                std::cout << "Message count: " << message_count << std::endl; // 打印消息计数器
+int L_(char data) {
+    return static_cast<unsigned char>(data);
+}
+
+// 四元数转欧拉角
+vector<double> quaternion_to_euler(vector<double> q, int degree_mode = 1) {
+    double qw = q[0], qx = q[1], qy = q[2], qz = q[3];
+    double roll = atan2(2 * (qw * qx + qy * qz), 1 - 2 * (qx * qx + qy * qy));
+    double pitch = asin(2 * (qw * qy - qz * qx));
+    double yaw = atan2(2 * (qw * qz + qx * qy), 1 - 2 * (qy * qy + qz * qz));
+
+    if (degree_mode == 1) {
+        roll = roll * 180.0 / M_PI;
+        pitch = pitch * 180.0 / M_PI;
+        yaw = yaw * 180.0 / M_PI;
+    }
+
+    return { roll, pitch, yaw };
+}
+
+// 欧拉角转四元数
+vector<double> euler_to_quaternion(vector<double> euler, int degree_mode = 0) {
+    double roll = euler[0], pitch = euler[1], yaw = euler[2];
+    if (degree_mode == 1) {
+        roll = roll * M_PI / 180.0;
+        pitch = pitch * M_PI / 180.0;
+        yaw = yaw * M_PI / 180.0;
+    }
+
+    double qx = sin(roll/2) * cos(pitch/2) * cos(yaw/2) - cos(roll/2) * sin(pitch/2) * sin(yaw/2);
+    double qy = cos(roll/2) * sin(pitch/2) * cos(yaw/2) + sin(roll/2) * cos(pitch/2) * sin(yaw/2);
+    double qz = cos(roll/2) * cos(pitch/2) * sin(yaw/2) - sin(roll/2) * sin(pitch/2) * cos(yaw/2);
+    double qw = cos(roll/2) * cos(pitch/2) * cos(yaw/2) + sin(roll/2) * sin(pitch/2) * sin(yaw/2);
+
+    return { qw, qx, qy, qz };
+}
+
+// 主函数
+int run(void *dora_context) {
+    imu.set_option(serial_port_base::baud_rate(115200));
+    vector<unsigned char> write_data = {0xA5, 0x5A, 0x04, 0x01, 0x05, 0xAA};
+    boost::asio::write(imu, boost::asio::buffer(write_data));
+
+    while (true) {
+        vector<unsigned char> data(40);
+        boost::asio::read(imu, boost::asio::buffer(data));
+        //printf("data size: %d\n", data.size());
+        if (data[0] == 0xA5 && data[1] == 0x5A && data.back() == 0xAA) {
+            double yaw_deg = (H_(data[3]) + L_(data[4])) / 10.0;
+
+            if (yaw_deg > 180.0) yaw_deg -= 360.0;
+            if (yaw_deg < -180.0) yaw_deg += 360.0;
+
+            yaw = degrees2rad * yaw_deg;
+            pitch = degrees2rad * (H_(data[7]) + L_(data[8])) / 10.0;
+            roll = degrees2rad * (H_(data[5]) + L_(data[6])) / 10.0;
+            imu_msg.linear_acceleration.x = 9.806 * (H_(data[9]) + L_(data[10])) / 16384.0;
+            imu_msg.linear_acceleration.y = 9.806 * (H_(data[11]) + L_(data[12])) / 16384.0;
+            imu_msg.linear_acceleration.z = 9.806 * (H_(data[13]) + L_(data[14])) / 16384.0;
+
+            imu_msg.angular_velocity.x = degrees2rad * (H_(data[15]) + L_(data[16])) / 32.8;
+            imu_msg.angular_velocity.y = degrees2rad * (H_(data[17]) + L_(data[18])) / 32.8;
+            imu_msg.angular_velocity.z = degrees2rad * (H_(data[19]) + L_(data[20])) / 32.8;            
+
+            euler = { roll, pitch, yaw };
+            vector<double> q = euler_to_quaternion(euler);
+
+            imu_msg.orientation.x = q[1];
+            imu_msg.orientation.y = q[2];
+            imu_msg.orientation.z = q[3];
+            imu_msg.orientation.w = q[0];
+
+            nlohmann::json json_obj = imu_msg.to_json();
+            std::string json_str = json_obj.dump();
+            printf("%s\n", json_str.c_str());
+            const char* char_ptr = json_str.c_str();
+            char* non_const_char_ptr = new char[json_str.size() + 1];
+            std::memcpy(non_const_char_ptr, char_ptr, json_str.size() + 1);
+
+            std::string out_id = "data";
+            int result = dora_send_output(dora_context, &out_id[0], out_id.length(), reinterpret_cast<char*>(non_const_char_ptr), json_str.size());
+            if (result != 0)
+            {
+                std::cerr << "failed to send output" << std::endl;
+                return 1;
             }
+            //cout << json_string << endl;
+
+            // 如果需要发送或处理该JSON数据
+            // const char* json_bytes = json_string.c_str();
+            // 发送 json_bytes 数据...
+        // printf("[c node] received event\n");
         }
-        else if (ty == DoraEventType_Stop)
-        {
-            printf("[c node] received stop event\n");
-            to_exit_process = true;
-        }
-        else
-        {
-            printf("[c node] received unexpected event: %d\n", ty);
-        }
- 
-        free_dora_event(event);
     }
- 
-    if (message_count >= max_messages)
-    {
-        std::cout << "Reached the maximum number of messages: " << max_messages << std::endl;
-        to_exit_process = true;
-    }
- 
-    if (IMU.isOpen()) {
-        IMU.close();
-    }
- 
+
     return 0;
 }
- 
 int main()
 {
     std::cout << "dora node A driver" << std::endl;
@@ -136,7 +142,6 @@ int main()
     auto ret = run(dora_context);
     free_dora_context(dora_context);
  
-    to_exit_process = true;
  
     std::cout << "exit dora node A ..." << std::endl;
     return ret;
